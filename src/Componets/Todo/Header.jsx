da41469b641bce361d00ae4719ac6b7a0e3a632a
sushilreact <sushilpal816@gmail.com>
2024-01-20 20:06:59 +0530

    secnf

--- src/Componets/Todo/Header.jsx
+++ src/Componets/Todo/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
   return <>
   <div className='container-fluid bg-dark'>
-   <div className='container'>
-    Header
+   <div className='container headerstyle '>
+    Todo App
    </div>
   </div>
   </>
--- src/Componets/Todo/Todoaction.jsx
+++ src/Componets/Todo/Todoaction.jsx
@@ -1,66 +1,73 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Todoaction = () => {
-    const [title, usetitle] = useState("")
-    const [desc, usedesc] = useState("")
-    const [collect, usecollect]= useState([])
+const [formval, useformval]=useState({
+title_form:'',
+desc_form:'',
+enddate_form:'',
+assign_form:''
 
-    const handleChange=(e)=>{
-     usetitle(e.target.value)
-    }
-    const handleChangedes=(e)=>{
+})
+const [records, setRecords]= useState([])
 
-        usedesc(e.target.value)
-    }
+const handleInputChange = (event)=>{
+const {name,value}= event.target
+useformval({...formval,[name]:value})
 
-const submitaction=(e)=>{
-    e.preventDefault()
-    usecollect([...collect, {title,desc}])
-    console.log(collect)
-    if(title==="")
-    {
-        alert("Please enter title")
-    }
-    usetitle("")
-    usedesc("")
 
 }
-let todoResult= "No Task"
 
-if(collect.length>0){
-    todoResult = collect.map((item,i)=>{
-        return <div className='row' >
-        <div className='col'>{item.title}</div>
-        <div className='col'>{item.desc}</div>
-        <div className='col' key={i}><button onClick={()=>{deletemet(i)}}>X</button></div>
-        </div>
-        })
-}
- 
+const submitHandle=(e)=>{
+e.preventDefault()
 
-const deletemet =(i)=>{
-let dublicate = [...collect]
-console.log(dublicate)
-dublicate.splice(i,1)
-usecollect(dublicate)
+const newRecords= {...formval, id:new Date().getTime().toString()}
+console.log(records)
+setRecords([...records,newRecords])
+
 }
 
-  return <>
- <div className='container'>
-    <div className='row'>
-        <form onSubmit={submitaction}>
-         <input type="text" className="form-control" value={title}  placeholder="enter title" onChange={handleChange}/>
-         <input type="text" className="form-control" value={desc}  placeholder="enter description" onChange={handleChangedes}/>
-         <input type="submit" value="Submit" />
 
-        </form>
+  return (
+    <div className='container-fluid'>
+      <div className='container todoinBui flexcolum'>
+       <div className='todoCard'>
+       
+       <form className='todoRwo' onSubmit={submitHandle}>
+       
+        <input type="text" name='title_form' value={formval.title_form} className='form-control'  placeholder='Enter Title' onChange={handleInputChange}/>
+        <textarea className='form-control' name='desc_form' value={formval.desc_form}  cols="30" rows="5" placeholder='Enter Descrption' onChange={handleInputChange}></textarea>
+        <input type="date" name='enddate_form' value={formval.enddate_form}  className='form-control' onChange={handleInputChange}/>
+        <select  className='form-select' name='assign_form' value={formval.assign_form} onChange={handleInputChange}>
+          <option value="Designer">Designer</option>
+          <option value="Developer">Developer</option>
+        </select>
+        <input type="submit" className='btn btn-success' value="Add Task" />
+        
+       </form>
+      
+       </div>
+
+<div className='todoshow'>
+{
+records.map((item)=>{
+  const {title_form,enddate_form,desc_form,assign_form,id}= item
+  return(
+    <div className='card' key={id}>
+<p>{title_form}</p>
+<p>{enddate_form}</p>
+<p>{desc_form}</p>
+<p>{assign_form}</p>
+
     </div>
+  )
+})
 
-  
-     {todoResult}
-   
- </div>
-  </>
+}
+</div>
+
+      </div>
+    </div>
+  )
 }
 
 export default Todoaction
